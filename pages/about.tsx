a27a0b0b0a7bf9e6f50d2c6711f0265f5aef76d8
MinhLe <minh.le@construct.sg>
2022-11-20 19:16:10 +0700

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,13 @@
 import Image from "next/image";
 
+import { PortableText } from "@portabletext/react";
 import Container from "components/Container";
 import SocialContact from "components/SocialContact";
-import avatar from "public/avatar.jpg";
-import ReactCertImage from "public/react-cert.png";
-import CssCertImage from "public/css-cert.png";
-import { getClient } from "lib/sanity-server";
-import { bioQuery } from "lib/queries";
-import { PortableText } from "@portabletext/react";
 import { avatarUrl } from "helpers/constants";
+import { bioQuery } from "lib/queries";
+import { getClient } from "lib/sanity-server";
 
 export default function About({ aboutBio }) {
-  console.log("aboutBio", aboutBio);
-
   return (
     <Container title="About – Minh Le">
       <div className="flex flex-col justify-center items-start max-2-xl md:max-w-5xl  mx-auto mb-16 w-full">
@@ -45,24 +40,26 @@ export default function About({ aboutBio }) {
               </p>
             </div>
 
-            <div className="flex items-center space-x-5">
-              <a href="/react-cert.png">
+            <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-5 space-y-5 sm:space-y-0">
+              <a href="/react-cert.png" target={"_blank"}>
                 <Image
                   src={`https://res.cloudinary.com/dlfxcpjvo/image/upload/v1668925514/portfolio/react-cert_s9c6mx.png`}
-                  width={200}
+                  width={250}
+                  height={150}
                   quality={100}
                   alt="cert-image"
-                  className="rounded-lg m-0"
+                  className="rounded-lg m-0 w-full sm:w-auto"
                 />
               </a>
 
-              <a href="/css-cert.png">
+              <a href="/css-cert.png" target={"_blank"}>
                 <Image
                   src={`https://res.cloudinary.com/dlfxcpjvo/image/upload/v1668925508/portfolio/css-cert_jnvdqo.png`}
-                  width={200}
+                  width={250}
+                  height={150}
                   quality={100}
                   alt="cert-image"
-                  className="rounded-lg m-0"
+                  className="rounded-lg m-0 w-full sm:w-auto"
                 />
               </a>
             </div>
