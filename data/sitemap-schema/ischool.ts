c3b45919192acd56e6902fcfe9fffde4748f90be
Minh Le <minh.le@construct.sg>
2022-12-01 17:09:55 +0700

    update sitemap schema

---
+++ data/sitemap-schema/ischool.ts
@@ -0,0 +1,128 @@
+import { githubPagesUrl } from "data/constant";
+
+export const ischoolSchema = {
+  name: "iSchool",
+  slug: "ischool",
+  sites: [
+    {
+      name: "About",
+      url: `${githubPagesUrl}/iSchool-Image/about.html`,
+    },
+    {
+      name: "Blog Classic",
+      url: `${githubPagesUrl}/iSchool-Image/blog-classic.html`,
+    },
+    {
+      name: "Blog Detail",
+      url: `${githubPagesUrl}/iSchool-Image/blog-detail.html`,
+    },
+    {
+      name: "Blog Grid Sidebar",
+      url: `${githubPagesUrl}/iSchool-Image/blog-grid-sidebar.html`,
+    },
+    {
+      name: "Blog List Sidebar",
+      url: `${githubPagesUrl}/iSchool-Image/blog-list-sidebar.html`,
+    },
+    {
+      name: "Contact",
+      url: `${githubPagesUrl}/iSchool-Image/contact.html`,
+    },
+    {
+      name: "Course Grid Left",
+      url: `${githubPagesUrl}/iSchool-Image/course-grid-left.html`,
+    },
+    {
+      name: "Course Grid None",
+      url: `${githubPagesUrl}/iSchool-Image/course-grid-none.html`,
+    },
+    {
+      name: "Course Grid Right",
+      url: `${githubPagesUrl}/iSchool-Image/course-grid-right.html`,
+    },
+    {
+      name: "Course List Left",
+      url: `${githubPagesUrl}/iSchool-Image/course-list-left.html`,
+    },
+    {
+      name: "Course List None",
+      url: `${githubPagesUrl}/iSchool-Image/course-list-none.html`,
+    },
+    {
+      name: "Course List Right",
+      url: `${githubPagesUrl}/iSchool-Image/course-list-right.html`,
+    },
+    {
+      name: "Error",
+      url: `${githubPagesUrl}/iSchool-Image/error.html`,
+    },
+    {
+      name: "Event Detail None",
+      url: `${githubPagesUrl}/iSchool-Image/event-detail-none.html`,
+    },
+    {
+      name: "Event Detail",
+      url: `${githubPagesUrl}/iSchool-Image/event-detail.html`,
+    },
+    {
+      name: "Event Grid Left",
+      url: `${githubPagesUrl}/iSchool-Image/event-grid-left.html`,
+    },
+    {
+      name: "Event Grid None",
+      url: `${githubPagesUrl}/iSchool-Image/event-grid-none.html`,
+    },
+    {
+      name: "Event List Left",
+      url: `${githubPagesUrl}/iSchool-Image/event-list-left.html`,
+    },
+    {
+      name: "Event List None",
+      url: `${githubPagesUrl}/iSchool-Image/event-list-none.html`,
+    },
+    {
+      name: "Gallery 1",
+      url: `${githubPagesUrl}/iSchool-Image/gallery-1.html`,
+    },
+    {
+      name: "Gallery 2",
+      url: `${githubPagesUrl}/iSchool-Image/gallery-2.html`,
+    },
+    {
+      name: "Gallery 3",
+      url: `${githubPagesUrl}/iSchool-Image/gallery-3.html`,
+    },
+    {
+      name: "Home Page",
+      url: `${githubPagesUrl}/iSchool-Image/index.html`,
+    },
+    {
+      name: "Home Page 2",
+      url: `${githubPagesUrl}/iSchool-Image/home2.html`,
+    },
+    {
+      name: "Home Page 3",
+      url: `${githubPagesUrl}/iSchool-Image/home3.html`,
+    },
+    {
+      name: "Single Course Left",
+      url: `${githubPagesUrl}/iSchool-Image/single-course-left.html`,
+    },
+    {
+      name: "Single Course No Sidebar",
+      url: `${githubPagesUrl}/iSchool-Image/single-course-nosidebar.html`,
+    },
+    {
+      name: "Single Course Right",
+      url: `${githubPagesUrl}/iSchool-Image/single-course-right.html`,
+    },
+    {
+      name: "Teacher Detail",
+      url: `${githubPagesUrl}/iSchool-Image/teacher-detail.html`,
+    },
+    {
+      name: "Teacher",
+      url: `${githubPagesUrl}/iSchool-Image/teacher.html`,
+    },
+  ],
+};
---
+++ data/sitemap-schema/kenhcuoi.ts
@@ -0,0 +1,120 @@
+import { githubPagesUrl } from "data/constant";
+
+export const kenhcuoiSchema = {
+  name: "Kenh Cuoi",
+  slug: "kenhcuoi",
+  sites: [
+    {
+      name: "Account Album",
+      url: `${githubPagesUrl}/KenhCuoi/account-album.html`,
+    },
+    {
+      name: "Account Info",
+      url: `${githubPagesUrl}/KenhCuoi/account-info.html`,
+    },
+    {
+      name: "Account Manage Album",
+      url: `${githubPagesUrl}/KenhCuoi/account-manage-album.html`,
+    },
+    {
+      name: "Account Manage Promotion",
+      url: `${githubPagesUrl}/KenhCuoi/account-manage-promotion.html`,
+    },
+    {
+      name: "Account Price List",
+      url: `${githubPagesUrl}/KenhCuoi/account-price-list.html`,
+    },
+    {
+      name: "Account Promotion",
+      url: `${githubPagesUrl}/KenhCuoi/account-promotion.html`,
+    },
+    {
+      name: "Account",
+      url: `${githubPagesUrl}/KenhCuoi/account.html`,
+    },
+    {
+      name: "Beauty Picture Detail",
+      url: `${githubPagesUrl}/KenhCuoi/beauty-pic-detail.html`,
+    },
+    {
+      name: "Beauty Picture NCC",
+      url: `${githubPagesUrl}/KenhCuoi/beauty-pic-ncc.html`,
+    },
+    {
+      name: "Beauty Picture",
+      url: `${githubPagesUrl}/KenhCuoi/beauty-pic.html`,
+    },
+    {
+      name: "Collection Detail 2",
+      url: `${githubPagesUrl}/KenhCuoi/collection-detail-2.html`,
+    },
+    {
+      name: "Collection Detail",
+      url: `${githubPagesUrl}/KenhCuoi/collection-detail.html`,
+    },
+    {
+      name: "Collection",
+      url: `${githubPagesUrl}/KenhCuoi/collection.html`,
+    },
+    {
+      name: "Idea Detail",
+      url: `${githubPagesUrl}/KenhCuoi/idea-detail.html`,
+    },
+    {
+      name: "Home Page",
+      url: `${githubPagesUrl}/KenhCuoi/index.html`,
+    },
+    {
+      name: "Login",
+      url: `${githubPagesUrl}/KenhCuoi/login.html`,
+    },
+    {
+      name: "Password Change Info",
+      url: `${githubPagesUrl}/KenhCuoi/password-change-info.html`,
+    },
+    {
+      name: "Password Change",
+      url: `${githubPagesUrl}/KenhCuoi/password-change.html`,
+    },
+    {
+      name: "Post List 1",
+      url: `${githubPagesUrl}/KenhCuoi/post-list1.html`,
+    },
+    {
+      name: "Post Video",
+      url: `${githubPagesUrl}/KenhCuoi/post-video.html`,
+    },
+    {
+      name: "Promotion Detail",
+      url: `${githubPagesUrl}/KenhCuoi/promotion-detail.html`,
+    },
+    {
+      name: "Promotion",
+      url: `${githubPagesUrl}/KenhCuoi/promotion.html`,
+    },
+    {
+      name: "Register NCC",
+      url: `${githubPagesUrl}/KenhCuoi/register-ncc.html`,
+    },
+    {
+      name: "Register",
+      url: `${githubPagesUrl}/KenhCuoi/register.html`,
+    },
+    {
+      name: "Service Detail",
+      url: `${githubPagesUrl}/KenhCuoi/service-detail.html`,
+    },
+    {
+      name: "Service",
+      url: `${githubPagesUrl}/KenhCuoi/service.html`,
+    },
+    {
+      name: "Share",
+      url: `${githubPagesUrl}/KenhCuoi/share.html`,
+    },
+    {
+      name: "Video",
+      url: `${githubPagesUrl}/KenhCuoi/video.html`,
+    },
+  ],
+};
