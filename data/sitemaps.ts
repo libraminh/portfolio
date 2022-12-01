4a2cd657684179bfdc9f65ca46fbcd8c9e0d775a
Minh Le <minh.le@construct.sg>
2022-12-01 18:34:00 +0700

    update schema

--- data/sitemaps.ts
+++ data/sitemaps.ts
@@ -1,8 +1,18 @@
+import { bookingAppSchema } from "./sitemap-schema/booking-app";
+import { bridalStudioSchema } from "./sitemap-schema/bridal-studio";
+import { flightHacksSchema } from "./sitemap-schema/flight-hacks";
 import { ischoolSchema } from "./sitemap-schema/ischool";
 import { kenhcuoiSchema } from "./sitemap-schema/kenhcuoi";
 import { magazineSchema } from "./sitemap-schema/magazine";
 
-export const siteMaps = [magazineSchema, kenhcuoiSchema, ischoolSchema];
+export const siteMaps = [
+  magazineSchema,
+  kenhcuoiSchema,
+  ischoolSchema,
+  bridalStudioSchema,
+  bookingAppSchema,
+  flightHacksSchema,
+];
 
 interface Site {
   name: string;
